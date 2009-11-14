c6031ed0fd2467d4219bb90c9a405dbe58e5e182
unknown <Martin@.(none)>
2009-11-14 19:44:59 +0100

    Basic clean up

--- app/helpers/attachments_helper.rb
+++ app/helpers/attachments_helper.rb
@@ -1,10 +1,10 @@
 module AttachmentsHelper
   def get_icon(filename)
     case filename[filename.size-3..filename.size]
-    when "pdf" then image_tag("/images/icon-pdf.png")
-    when "doc" then image_tag("/images/icon-doc.png")
-    when "ppt" then image_tag("/images/icon-ppt.png")
-    else image_tag("/images/icon-xxx.png")
+    when "pdf" then image_tag("/images/mime-types/icon-pdf.png")
+    when "doc" then image_tag("/images/mime-types/icon-doc.png")
+    when "ppt" then image_tag("/images/mime-types/icon-ppt.png")
+    else image_tag("/images/mime-types/icon-xxx.png")
     end
   end
 end
